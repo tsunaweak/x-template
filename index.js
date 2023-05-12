11405f87860b817d08ae1b0c576fc5182e872047
tsunaweak <tsunaweak@gmail.com>
2023-05-12 17:51:22 +0800

    uploading of x-template

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const Path = require('path');
+
+const mustache = require('./lib/mustache');
+const templateInjector = require('./lib/template-injector');
+
+function index(req, res, next){
+    process.env.xviews = req.app.get('views') || 'views';
+    res.render = (tmpl, data = {}) => {
+        try {
+            const filePath = Path.join(process.env.xviews, `${tmpl}.x`);
+            let template = templateInjector(fs.readFileSync(filePath, 'utf8'));
+            template = mustache(template, data);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(template); 
+        } catch (err) {
+          next(err);
+        }
+    };
+    
+    next();
+}
+
+
+
+
+module.exports = index
+
+
